2fdad8189f4ca990b6bb02fde1bdc8cfe5c8fed8
Mayra Torres <isemayratorres18@gmail.com>
2023-02-23 23:04:43 -0500

    [ProgramaciónReactiva] Add Validators en editaralumno-dialog - Angular Material

--- src/app/components/editar-alumno-dialog/editar-alumno-dialog.component.ts
+++ src/app/components/editar-alumno-dialog/editar-alumno-dialog.component.ts
@@ -9,18 +9,18 @@ import { Alumno } from 'src/app/models/alumno';
   styleUrls: ['./editar-alumno-dialog.component.css']
 })
 export class EditarAlumnoDialogComponent implements OnInit{
-  formulario: FormGroup;
+  /*formulario: FormGroup;*/
 
   constructor(
     private dialogRef: MatDialogRef<EditarAlumnoDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Alumno                /*No olvidar que "data" tiene que ser PUBLIC */
   ){
-    this.formulario = new FormGroup({
+    /*this.formulario = new FormGroup({
       nombre: new FormControl(data.nombre),
       edad: new FormControl(data.edad),
       curso: new FormControl(data.curso.nombre),
       profesor: new FormControl(data.profesor.nombre)
-    })
+    })*/
   }                                                           /*"Alumno" para definir estructura o puede ser "any"*/
 
   ngOnInit(): void {
@@ -30,11 +30,12 @@ export class EditarAlumnoDialogComponent implements OnInit{
 
   //Validación
   public editAlumnoFormGroup = new FormGroup({
-    nombre: new FormControl(this.data.nombre, [Validators.required, Validators.minLength(10)]),
-    edad: new FormControl(this.data.edad, [Validators.required, Validators.minLength(3)]),
-    curso: new FormControl(this.data.curso.nombre, [Validators.required, Validators.minLength(16)]),
-    profesor: new FormControl(this.data.profesor.nombre, [Validators.required, Validators.minLength(10)])
-  });
+    nombre: new FormControl(this.data.nombre, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]),
+    apellido: new FormControl(this.data.apellido, [Validators.required, Validators.minLength(2), Validators.maxLength(10)]),
+    edad: new FormControl(this.data.edad, [Validators.required, Validators.minLength(1)]),
+    curso: new FormControl(this.data.curso.nombre, [Validators.required, Validators.minLength(4)]),
+    profesor: new FormControl(this.data.profesor.nombre, [Validators.required, Validators.minLength(2), Validators.maxLength(10)])
+  })as FormGroup;
 
   public guardar(): void{
     this.dialogRef.close(this.editAlumnoFormGroup.value);
@@ -44,4 +45,15 @@ export class EditarAlumnoDialogComponent implements OnInit{
     this.dialogRef.close(null);
   }
   
+  public validateMinLength(requiredControl: string): boolean {
+    return this.editAlumnoFormGroup.controls[requiredControl].hasError('minlength') && !this.editAlumnoFormGroup.controls[requiredControl].hasError('required');
+  }
+
+  public validateRequired(requiredControl: string): boolean {
+    return this.editAlumnoFormGroup.controls[requiredControl].hasError('required');
+  }
+
+  public validateMaxLength(requiredControl: string): boolean {
+    return this.editAlumnoFormGroup.controls[requiredControl].hasError('maxlength') && !this.editAlumnoFormGroup.controls[requiredControl].hasError('required');
+  }
 }
